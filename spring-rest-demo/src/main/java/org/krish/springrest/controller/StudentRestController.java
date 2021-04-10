c9e6e1f7c17dae90b763ca548fd45eeb73bc153f
Krish <krishnendu.gupta00@gmail.com>
2021-04-10 19:26:44 +0530

    Exception handling added - StudentNotFoundException for Student API

--- spring-rest-demo/src/main/java/org/krish/springrest/controller/StudentRestController.java
+++ spring-rest-demo/src/main/java/org/krish/springrest/controller/StudentRestController.java
@@ -1,10 +1,11 @@
 package org.krish.springrest.controller;
 
+import org.krish.springrest.custom.error.response.StudentErrorResponse;
+import org.krish.springrest.custom.exception.StudentNotFoundException;
 import org.krish.springrest.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -37,6 +38,30 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable("studentId") int sid)
     {
+        if(sid <= 0 || sid > sList.size())
+            throw new StudentNotFoundException("Student with id "+sid+" doesn't exist");
+
         return sList.get(sid - 1);  //sList is 0 based.
+    }
+
+    //Add an exception handler using @ExceptionHandler
+
+    /*
+     *
+     * @ExceptionHandler - tells Spring that this method is an exception handler.
+     * ResponseEntity<StudentErrorResponse> - type of the response body.
+     * StudentNotFoundException -   is the exception type to catch
+     */
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e)
+    {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());  //404
+        error.setTimestamp(System.currentTimeMillis()); //current timestamp
+
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);    //Jackson will automatically convert StudentErrorResponse POJO and
+                                                                    // convert to JSON.
     }
 }
